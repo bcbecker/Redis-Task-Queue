8d8416f49737dee132086a1829e78d29f535ece5
Bryce Becker <bryce.becker15@gmail.com>
2021-09-29 13:16:16 -0400

    Update app.py
    Use import string instead of importing  each file and calling path to task function

--- task_queue/app.py
+++ task_queue/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request
 from redis import Redis
-#from celery import Celery
 from rq import Queue
-from utils import simulated_task
 
 app = Flask(__name__)
 
@@ -13,10 +11,11 @@ q = Queue(connection=r)
 @app.route("/task")
 def add_task_to_queue():
 
-    n = request.args.get("n")
+    n = request.args.get('n')
 
     if n:
-        job = q.enqueue(simulated_task, n)
+        #use import string as first arg
+        job = q.enqueue('task_queue.tasks.simulated_task', n)
         
         return f'{job.id} added to queue at {job.enqueued_at} ------ {len(q)} tasks in queue'
     return "No task to add"
@@ -24,4 +23,4 @@ def add_task_to_queue():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
